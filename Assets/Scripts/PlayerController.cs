feecba7571441b289467f2d51b3269b1ba91d141
Failattu <kalle.sirkesalo@eficode.com>
2016-01-30 17:32:13 +0200

    Fading out

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,7 @@ using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
-
+    public Texture blackTexture;
     public float speed;
     public Text countText;
     public Text winText;
@@ -13,34 +13,66 @@ public class PlayerController : MonoBehaviour
     public GameObject player;
     private AudioSource audio;
     private bool death = false;
-    
+    private bool fadingIn;
+    private bool fadingOut;
+    private float fadeLength;
+    float alphaFadeValue = 1;
 
     void Start()
     {
+        fadingIn = false;
+        fadingOut = false;
+        fadeLength = 20;
         audio = GetComponent<AudioSource>();
         rb = GetComponent<Rigidbody>();
         count = 0;
         SetCountText();
         winText.text = "";
+        
     }
 
     void FixedUpdate()
     {
         if (audio.isPlaying == false && death == true) {
             player.SetActive(false);
-
         }
-        if (death == false)
-        {
-            float moveHorizontal = Input.GetAxis("Horizontal");
-            float moveVertical = Input.GetAxis("Vertical");
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
 
-            Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
-            rb.AddForce(movement * speed);
-        }
+        rb.AddForce(movement * speed);
+        
     }
-    void OnTriggerEnter(Collider other)
+    private void OnGUI()
+    {
+        if (death == true)
+        {
+            if (fadingIn)
+            {
+                alphaFadeValue -= Time.deltaTime / fadeLength;
+                if (alphaFadeValue < 0)
+                {
+                    fadingIn = false;
+                    alphaFadeValue = 0;
+                }
+            }
+            if (fadingOut)
+            {
+                alphaFadeValue += Time.deltaTime / fadeLength;
+                if (alphaFadeValue > 1)
+                {
+                    fadingOut = false;
+                    alphaFadeValue = 1;
+                }
+            }
+            GUI.color = new Color(0, 0, 0, alphaFadeValue);
+            GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), blackTexture);
+        
+    }
+    }
+
+        void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Finish"))
         {
@@ -53,11 +85,12 @@ public class PlayerController : MonoBehaviour
     {
         countText.text = "Count: " + count.ToString();
         if (count > 0)
-        { 
+        {
             winText.text = "You have sacrificed yourself!";
             death = true;
             audio.Play();
-            
+            fadingOut = true;
+
         }
     }
 }
